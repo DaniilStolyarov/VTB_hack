408776085db0f49a683c572ab487bf5c964a9870
danya <danstolyarov79@gmail.com>
2023-10-14 01:22:03 +0300

    test

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.post('/destination', express.json(), async (req, res) =>
         return;
     }
     const answer = await db.filterPoints(req.body.criteria);
-    res.json(JSON.stringify(answer.rows))
+    res.json(answer.rows)
     console.log(req.body)
     console.log('answer: ')
     console.log(JSON.stringify(answer.rows))
