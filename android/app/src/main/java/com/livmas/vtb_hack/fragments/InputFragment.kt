d52c97588045cd14c77edbd9c830e9c3f8a149a6
livmas <seiranyan.sg@gmail.com>
2023-10-14 20:01:26 +0300

    design update

--- android/app/src/main/java/com/livmas/vtb_hack/fragments/InputFragment.kt
+++ android/app/src/main/java/com/livmas/vtb_hack/fragments/InputFragment.kt
@@ -10,13 +10,13 @@ import androidx.core.view.get
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.clearFragmentResult
 import com.livmas.vtb_hack.MainActivity
+import com.livmas.vtb_hack.R
 import com.livmas.vtb_hack.connection.HttpClient
 import com.livmas.vtb_hack.databinding.FragmentInputBinding
 import com.livmas.vtb_hack.toInt
 
 class InputFragment : DialogFragment() {
 
-    private lateinit var viewModel: InputViewModel
     private lateinit var binding: FragmentInputBinding
     private lateinit var client: HttpClient
 
@@ -35,19 +35,20 @@ class InputFragment : DialogFragment() {
         }
 
         return AlertDialog.Builder(activity)
-            .setMessage("Выберете параментры поиска")
+            .setMessage(R.string.settings_message)
             .setView(binding.root)
-            .setPositiveButton("Подтвердить") { _, _ ->
+            .setPositiveButton(R.string.submit) { _, _ ->
                 mainActivity.holder.location?.let {loc ->
 
                     var criteria = ""
-                    for (i in binding.llCriterias.children) {
+                    for (i in binding.llCriterias.children)
                         criteria += (i as CheckedTextView).isChecked.toInt().toString()
-                    }
+
                     client.query(loc, criteria)
                     Log.d("http", criteria.length.toString())
                 }
             }
+            .setNegativeButton(R.string.cancel) {_, _ -> }
             .show()
 
     }
